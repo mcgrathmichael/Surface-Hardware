e33d7105b7355c863a3304ef235af7164bdc5a97
mcgrathmichael <corecreate0@gmail.com>
2026-07-13 14:48:21 +0200

    Fix MQTT authentication and discovery

--- surface_hardware/run.sh.sh
+++ surface_hardware/run.sh.sh
@@ -4,6 +4,7 @@ export MQTT_HOST=$(bashio::services mqtt "host")
 export MQTT_PORT=$(bashio::services mqtt "port")
 export MQTT_USER=$(bashio::services mqtt "username")
 export MQTT_PASSWORD=$(bashio::services mqtt "password")
+
 echo "Starting Surface Hardware"
 
 python3 /hardware.py
